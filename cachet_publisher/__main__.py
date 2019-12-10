63c8cd380633e96aad7de2998288ee375bca3e2a
Michael Blättler <michael.blaettler@innetag.ch>
2019-12-10 15:27:15 +0100

    feat(setup): add proper setup.py to easily install the package on the sensu master/agent

---
+++ cachet_publisher/__main__.py
@@ -0,0 +1,9 @@
+from cachet_publisher import cachet_publisher
+
+
+def main():
+    cachet_publisher.CachetHandler(True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name="sensu-cachet-handler",
+      version="0.0.1",
+      description="An application to publish your sensu check results to cachet",
+      install_requires=[
+          "cachet-client",
+          "sensu-plugin"
+      ],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Michael Blättler",
+      author_email="michael.blaettler@innetag.ch",
+      packages=find_packages(),
+      zip_safe=False,
+      entry_points={
+          "console_scripts": [
+              "cachet_publisher=cachet_publisher.__main__:main"
+          ]}
+      )
